e4e4e7e9ee642a503525ea6974f4941462adb458
Dipak-Dhariyaparmar <dipakdhariyaparmar@gmail.com>
2025-04-13 12:47:54 +0530

    refactor: update HTML structure and improve SEO meta tags

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import UserContext from './context/UserContext.jsx';
-import CaptainContext from './context/CapatainContext.jsx';
-import SocketProvider from './context/SocketContext.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import UserContext from "./context/UserContext.jsx";
+import CaptainContext from "./context/CapatainContext.jsx";
+import SocketProvider from "./context/SocketContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById("root")).render(
   <CaptainContext>
     <UserContext>
       <SocketProvider>
@@ -18,5 +17,6 @@ createRoot(document.getElementById('root')).render(
       </SocketProvider>
     </UserContext>
   </CaptainContext>
-
-)
+);
+//not used a redux toolkit because it is not needed for this project
+//and it is a simple project and react context is enough for this project
