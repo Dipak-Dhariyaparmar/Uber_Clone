4f41bc83ab578c324fcb94cbbcbe016fb233a5a7
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-06-15 10:42:17 +0530

    Update Riding.jsx

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom"; // Added useLocation
+import { Link, useLocation } from "react-router-dom"; 
 import { useEffect, useContext } from "react";
 import { SocketContext } from "../context/SocketContext";
 import { useNavigate } from "react-router-dom";
@@ -7,7 +7,7 @@ import LiveTracking from "../components/LiveTracking";
 
 const Riding = () => {
   const location = useLocation();
-  const { ride } = location.state || {}; // Retrieve ride data from a location
+  const { ride } = location.state || {}; 
   const { socket } = useContext(SocketContext);
   const navigate = useNavigate();
 
@@ -73,3 +73,4 @@ const Riding = () => {
 };
 
 export default Riding;
+// This component is related to riding
