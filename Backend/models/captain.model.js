f2c83bb80bf0ae985a643f968c47332f2e2e2300
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-07-03 21:46:42 +0530

    Update captain.model.js

--- Backend/models/captain.model.js
+++ Backend/models/captain.model.js
@@ -87,4 +87,3 @@ captainSchema.statics.hashPassword = async function (password) {
 const captainModel = mongoose.model("captain", captainSchema);
 
 module.exports = captainModel;
-// This model is a captain related
