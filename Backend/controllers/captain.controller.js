40e6f92bc7906510cbe5074dd1f5f404abad7d80
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-09-17 23:47:43 +0530

    Update captain.controller.js

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,4 +1,4 @@
-// This component is related to captains
+// This component is related to captain
 
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
